f024c3dc4ee0c1bae7ee9f1691321a099aff18fb
170030002 <59111097+170030002@users.noreply.github.com>
2020-01-04 14:34:52 +0530

    Add files via upload

--- 2a.java
+++ 2a.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class 2a
+class CheckEvenOdd
 {
   public static void main(String args[])
   {
@@ -8,8 +8,8 @@ class 2a
     System.out.println("Enter an Integer number:");
 
     //The input provided by user is stored in num
-   
-    num = 10
+    Scanner input = new Scanner(System.in);
+    num = input.nextInt();
 
     /* If number is divisible by 2 then it's an even number
      * else odd number*/
